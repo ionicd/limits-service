4d5e2f9e73101b62e8d01c572953e4f4bcf8dd28
Saurav <Saurav.KC14@myhunter.cuny.edu>
2020-08-13 14:28:23 -0400

    Spring cloud git set

--- src/main/java/com/javatpoint/limitsservice/LimitsConfigurationController.java
+++ src/main/java/com/javatpoint/limitsservice/LimitsConfigurationController.java
@@ -1,14 +1,19 @@
 package com.javatpoint.limitsservice;
 
+import org.springframework.beans.factory.annotation.Autowired;  
 import org.springframework.web.bind.annotation.GetMapping;  
 import org.springframework.web.bind.annotation.RestController;  
-import com.javatpoint.limitsservice.bean.LimitConfiguration;
+import com.javatpoint.limitsservice.bean.LimitConfiguration;  
 
 @RestController  
-public class LimitsConfigurationController
+public class LimitsConfigurationController   
 {  
+	@Autowired    
+	private Configuration configuration;      
 	@GetMapping("/limits")  
-	public LimitConfiguration retriveLimitsFromConfigurations(){  
-		return new LimitConfiguration(1000, 1);  
+	public LimitConfiguration retriveLimitsFromConfigurations()  
+	{  
+		//getting values from the properties file  
+		return new LimitConfiguration(configuration.getMaximum(), configuration.getMinimum());  
 	}  
 }  
