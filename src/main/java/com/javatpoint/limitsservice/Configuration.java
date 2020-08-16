f2110fb4706fdcfd652bddf9ee6ee18c567c9bcf
Saurav <Saurav.KC14@myhunter.cuny.edu>
2020-08-15 23:07:51 -0400

    Added spring cloud config

--- src/main/java/com/javatpoint/limitsservice/Configuration.java
+++ src/main/java/com/javatpoint/limitsservice/Configuration.java
@@ -1,9 +1,9 @@
-package com.javatpoint.limitsservice;
+ package com.javatpoint.limitsservice;
 import org.springframework.boot.context.properties.ConfigurationProperties;  
 import org.springframework.stereotype.Component;
 
-@Component  
-@ConfigurationProperties("limits-service")  
+@Component
+@ConfigurationProperties("limits-service")
 public class Configuration   
 {  
 	private int maximum;  
--- src/main/java/com/javatpoint/limitsservice/bean/LimitsConfigurationController.java
+++ src/main/java/com/javatpoint/limitsservice/bean/LimitsConfigurationController.java
@@ -1,5 +1,20 @@
 package com.javatpoint.limitsservice.bean;
+import org.springframework.beans.factory.annotation.Autowired;  
+import org.springframework.web.bind.annotation.GetMapping;  
+import org.springframework.web.bind.annotation.RestController;
 
-public class LimitsConfigurationController {
+import com.javatpoint.limitsservice.Configuration;
+import com.javatpoint.limitsservice.bean.LimitConfiguration;  
 
+@RestController  
+public class LimitsConfigurationController   
+{  
+@Autowired    
+private Configuration configuration;      
+@GetMapping("/limits")  
+public LimitConfiguration retriveLimitsFromConfigurations()  
+{  
+//getting values from the properties file  
+return new LimitConfiguration(configuration.getMaximum(), configuration.getMinimum());  
+}  
 }
